fd6e29cd6ba627917ee872dd2be5aa7b5641e9e3
AlyanQureshi <144194260+AlyanQureshi@users.noreply.github.com>
2023-12-03 22:15:46 -0700

    Add files via upload

---
+++ Museum3.sql
@@ -0,0 +1,156 @@
+drop database if exists museum;
+create database museum;
+use museum;
+
+create table ART_OBJECT
+(obj_ID char(5) not null,
+title varchar(50) not null,
+descript varchar(50) not null,
+year_created int not null,
+origin varchar(20) not null,
+epoch varchar(20) not null,
+collection_name varchar(25) not null,
+artist_name varchar(25),
+exhibit_ID char(5) not null,
+
+constraint AOPK primary key (obj_ID));
+
+create table PAINTING
+(obj_ID char(5) not null,
+paint_type varchar(20) not null,
+drawn_on varchar(20) not null,
+style varchar(20) not null,
+
+constraint PPK primary key (obj_ID),
+constraint PFK foreign key (obj_ID) references ART_OBJECT(obj_ID));
+
+create table SCULPTURE
+(obj_ID char(5) not null,
+material varchar(20) not null,
+height_meter varchar(10) not null,
+weight_Kg varchar(20) not null,
+style varchar(20) not null,
+
+constraint SCPK primary key (obj_ID),
+constraint SCFK foreign key (obj_ID) references ART_OBJECT(obj_ID));
+
+create table STATUE
+(obj_ID char(5) not null,
+material varchar(20) not null,
+height_meter varchar(10) not null,
+weight_Kg varchar(20) not null,
+style varchar(20) not null,
+
+constraint STPK primary key (obj_ID),
+constraint STFK foreign key (obj_ID) references ART_OBJECT(obj_ID));
+
+create table OTHER
+(obj_ID char(5) not null,
+Otype varchar(20) not null,
+style varchar(20) not null,
+
+constraint SPK primary key (obj_ID),
+constraint OFK foreign key (obj_ID) references ART_OBJECT(obj_ID));
+
+create table COLLECTIONS
+(collection_name varchar(25) not null,
+Ctype varchar(20) not null,
+descript varchar(50) not null,
+address varchar(30) not null,
+phone char(12) not null,
+current_contact varchar(20) not null,
+
+constraint CPK primary key (collection_name));
+
+create table BORROWED_COLLECTION
+(borrowed_from varchar(25) not null,
+date_returned date,
+date_borrowed date not null,
+
+constraint BCPK primary key (borrowed_from),
+constraint BCFK foreign key (borrowed_from) references COLLECTIONS(collection_name));
+
+create table PERMANENT_COLLECTION
+(collection_name varchar(25) not null,
+Pstatus varchar(10) not null,
+cost int not null,
+date_acquired date not null,
+
+constraint PCPK primary key (collection_name),
+constraint PCFK foreign key (collection_name) references COLLECTIONS(collection_name));
+
+create table ARTIST
+(artist_name varchar(25) not null,
+descript varchar(50),
+date_born date,
+date_died date,
+country_of_orgin varchar(20) not null,
+style varchar(20) not null,
+epoch varchar(20) not null,
+
+constraint ARTPK primary key (artist_name));
+
+create table EXHIBITION
+(exhibit_ID char(5) not null,
+Ename varchar(20) not null,
+start_date date not null,
+end_date varchar(10) not null,
+
+constraint EXID primary key (exhibit_ID));
+
+insert into EXHIBITION
+values 	('90001', 'Cecily Brown', '2023-12-03', '2023-12-10'), -- Other
+		('90002', 'Manet Degas', '2023-12-11', '2023-12-18'), -- Painting
+        ('90003', 'Africa & Byzantium', '2023-12-19', '2023-12-27'), -- Statue
+		('90004', 'Tentacrome', '2020-11-30', 'Ongoing'); -- Sculpture
+
+insert into ART_OBJECT
+values  ('10001', 'Martyrdom of the 7', 'A narrative of religious persecution.', 1530, 'Flemish', 'Renaissance', 'Fletcher Collection', 'Dirck Vellert', '90001'),
+		('10002', 'Tric Trac Gameboard', 'Gameboards served as diplomatic gifts.', 1550, 'German', 'Renaissance', 'Fletcher Collection', 'David Schwartz', '90001'),
+        ('10003', 'The Death of Dave', 'Showcases the last moments of St.Dave.', 1250, 'Greek', 'Medieval', 'Brown Collection', 'Alexander Antacoumpo', '90002'),
+        ('10004', 'The Victory of Valhalla', 'Displays the warriors of Valhalla', 793, 'Scandavavian', 'Viking Age', 'Brown Collection', null, '90002'),
+        ('10005', 'The Statue of Eren', 'A leading Monarch for the People.', 1790, 'Japanese', 'Industrial', 'Taisho Collection', null, '90003'),
+        ('10006', 'The Statue of Alexander the Great', 'King of Macedon', 320, 'Greek', 'Ancient', 'Contrast Collection', null, '90003'),
+        ('10007', 'The Bust of Nefertiti', 'Great Royal Wife of Egyptian pharaoh Akhenaten', 350, 'Egypt', 'Ancient', 'Contrast Collection', 'Thutmoze', '90004'),
+		('10008', 'Guitar', 'An instrument of great influence.', 1910, 'Spain', 'Machine Age', 'Taisho Collection', null, '90004');
+
+insert into PAINTING
+values  ('10003', 'Oil', 'Cotton', 'Realism'),
+		('10004', 'Incoustic', 'Silk', 'Impressionism');
+
+insert into SCULPTURE
+values  ('10007', 'Stone', '0.5', '20', 'Round'),
+		('10008', 'Marble', '1.1', '40', 'Musical');
+
+insert into STATUE
+values  ('10005', 'Stone', '4', '240', 'Carved'),
+		('10006', 'Ivory', '6', '640', 'Carved');
+
+insert into OTHER
+values  ('10001', 'Stained Glass', 'Glass Stained'),
+		('10002', 'Gameboard', 'Entertainment');
+
+insert into COLLECTIONS
+values  ('Fletcher Collection', 'Museum', 'Collection of pieces regarding personal interests.', '731 Fondren, Houston TX', '111-222-3333', 'James Fletcher'),
+		('Brown Collection', 'Museum', 'Collection of pieces regarding past wars.', '291 Berry, Bellaire TX', '222-333-4444', 'Bruce Brown'),
+        ('Taisho Collection', 'Personal', 'Collection of pieces regarding music and Monarchs', '5631 Rice, Houston TX', '333-444-5555', 'Nick Suzuki'),
+        ('Contrast Collection', 'Personal', 'Collection of pieces regarding Ancient Times', '980 Dallas, Houston TX', '444-555-6666', 'Henry Bucklemore');
+
+insert into BORROWED_COLLECTION
+values  ('Taisho Collection', '2023-07-21', '2020-01-13'),
+		('Contrast Collection', null, '2015-09-22');
+
+insert into PERMANENT_COLLECTION
+values  ('Fletcher Collection', 'On Display', '30000', '2002-08-19'),
+		('Brown Collection', 'Stored', '81500', '2010-03-10');
+
+insert into ARTIST
+values  ('Dirck Vellert', 'World-Renouned Artist of the Renaissance', '1489-05-04', '1559-12-03', 'Belgium', 'Futurism', 'Renaissance'),
+		('David Schwartz', 'Small German Artist of the Renaissance', null, null, 'Germany', 'Rococo', 'Renaissance'),
+        ('Alexander Antacoumpo', 'Famed Painter of the Medieval times', '1220-02-24', '1287-01-31', 'Greece', 'Classicism', 'Medieval'),
+        ('Thutmoze', null, null, null, 'Egypt', 'Symbolism', 'Medieval');
+
+alter table ART_OBJECT
+	add constraint CFK foreign key (collection_name) references COLLECTIONS(collection_name),
+	add constraint AFK foreign key (artist_name) references ARTIST(artist_name),
+	add constraint EIDFK foreign key (exhibit_ID) references EXHIBITION(exhibit_ID);
