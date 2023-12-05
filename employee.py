512f701551b63eec67443b06d05a9e49123312b9
jaybarc <144380640+jaybarc@users.noreply.github.com>
2023-12-05 02:12:01 -0700

    removes artist then object (need edit/add still)

---
+++ employee.py
@@ -0,0 +1,86 @@
+import mysql.connector
+
+def employeeMenu(connection):
+    while True:
+        print("\n************************************************************************************************************************\n")
+        print("Welcome to the Employee Menu:")
+        print("1.) Add Information")
+        print("2.) Remove Information")
+        print("3.) Edit Information")
+
+        print("\n0. Exit\n")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            addInfo(connection)
+        elif choice == '2':
+            removeInfo(connection)
+        elif choice == '3':
+            editInfo(connection)
+        elif choice == '0':
+            print("Exiting employee menu.")
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
+def addInfo(connection):
+    print("add obj")
+
+def removeInfo(connection):
+    try:
+        cursor = connection.cursor()
+        display_query = "SELECT obj_ID, title FROM art_object"
+        cursor.execute(display_query)
+        artworks = cursor.fetchall()
+
+        print("\nArt Objects:")
+        for artwork in artworks:
+            print(f"Artwork ID: {artwork[0]}, {artwork[1]}")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        print("Failed to display Art Objects.")
+        return
+
+    print("\nRemoving Art Object:")
+    object_id = input("Enter Art Object ID to remove: ")
+    try:
+        cursor = connection.cursor()
+
+        check_artist_query = f"SELECT artist_name FROM artist WHERE obj_ID = {object_id}"
+        cursor.execute(check_artist_query)
+        artists = cursor.fetchall()
+
+        if artists:
+            print(f"Associated artist found. Deleting associated artist(s) first...")
+
+            for artist in artists:
+                artist_name = artist[0]
+                deleteQuery = f"DELETE FROM artist WHERE artist_name = '{artist_name}'"
+                cursor.execute(deleteQuery)
+                connection.commit()
+
+                if cursor.rowcount > 0:
+                    print(f"Artist {artist_name} deleted successfully.")
+                else:
+                    print(f"Failed to delete artist {artist_name}.")
+
+        deleteQuery = f"DELETE FROM art_object WHERE obj_ID = {object_id}"
+        cursor.execute(deleteQuery)
+        connection.commit()
+
+        if cursor.rowcount > 0:
+            print("Art Object removed successfully.")
+        else:
+            print(f"No Art Object found with ID {object_id}.")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        print("Failed to remove Art Object, returning to Main Menu.")
+
+
+
+def editInfo(connection):
+    # Implement the logic to edit an existing art object
+    pass
